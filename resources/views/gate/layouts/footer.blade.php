6b6f92629588006ddb9927a922417908cd267033
Roger Wu (吳偉召) <roger@icarry.me>
2024-11-08 12:41:21 +0800

    修改

--- resources/views/gate/layouts/footer.blade.php
+++ resources/views/gate/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 {{-- Main Footer --}}
     <footer class="main-footer bg-navy text-sm">
         <div class="float-right d-none d-sm-inline-block">
-            Copyright &copy; {{ date('Y') }} <strong><a href="https://rvt.idv.tw" title="Roger's CodingLab" target="_blank">Roger's CodingLab.</a> </strong>All rights reserved.
+            Copyright &copy; {{ date('Y') }} <strong><a href="https://roger.rvt.idv.tw" title="Roger's CodingLab" target="_blank">Roger's CodingLab.</a> </strong>All rights reserved.
         </div>
     </footer>
